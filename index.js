6d20e5f32d0b1e908a9c724319659cedc6325c85
RosheedatOyindamola <Rosheedat1234@yahoo>
2023-03-17 13:20:52 +0100

    Add all todo App files

---
+++ index.js
@@ -0,0 +1,50 @@
+const listContainer = document.querySelector("#list-Container");
+const form = document.querySelector(".todo-Form");
+const  input = document.querySelector(".todo-input");
+const addButton = document.querySelector("#add");
+
+let todosItem = [ ];
+function addItems(){
+      
+        const text = input.value;
+      if ( text !== ""){
+        todosItem.unshift(text); 
+    
+        input.value="";
+      input.setAttribute( "placeholder", "Add new task...")
+      localStorage.setItem("myTodoItem", JSON.stringify(todosItem));
+    
+      } else{
+        alert("You need to enter your task")
+      }
+       
+      displayItem(text);
+        console.log(todosItem);
+   
+}
+function displayItem(text, button){
+
+    const todoItemElement = document.createElement("li");
+    const textNode = document.createTextNode(text);
+    const deleteItemButton = document.createElement("button");
+    deleteItemButton.innerText = "x";
+    todoItemElement.appendChild(textNode);
+    todoItemElement.appendChild(deleteItemButton);
+    listContainer.appendChild(todoItemElement);
+
+
+  listContainer.insertBefore(todoItemElement, listContainer.children[0]);
+ 
+ 
+  deleteItemButton.addEventListener("click",  deleteItem)
+  
+}
+function deleteItem(){
+console.log(this)
+console.log(this.parentElement);
+this.parentElement.remove();
+
+}
+addButton.addEventListener("click", addItems);
+
+ 
